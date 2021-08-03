8db0dbefc3c35bcf1dabe80969cd2c51149ccbd9
Ckyiu <UnsignedArduino@outlook.com>
2021-08-02 21:04:00 -0400

    Make a cursor

---
+++ images.g.ts
@@ -0,0 +1,41 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "JWx@oFVVe#czFjG%4e3D":
+            case "cursor":return img`
+f . . . . . . . . . 
+f f . . . . . . . . 
+f 1 f . . . . . . . 
+f 1 1 f . . . . . . 
+f 1 1 1 f . . . . . 
+f 1 1 1 1 f . . . . 
+f 1 1 1 1 1 f . . . 
+f 1 1 1 1 1 1 f . . 
+f 1 1 1 1 1 1 1 f . 
+f 1 1 f 1 f f f f f 
+f 1 f f 1 f . . . . 
+f f . . f 1 f . . . 
+f . . . f 1 f . . . 
+. . . . . f 1 f . . 
+. . . . . f 1 f . . 
+. . . . . . f . . . 
+`;
+            case "68th#X[:^@J:pK6;!p@%":
+            case "cursor_pointer":return img`
+f 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+function update_cursor () {
+    sprite_cursor.top = sprite_cursor_pointer.top
+    sprite_cursor.left = sprite_cursor_pointer.left
+}
+function enable_cursor_movement (en: boolean) {
+    if (en) {
+        controller.moveSprite(sprite_cursor_pointer, 75, 75)
+    } else {
+        controller.moveSprite(sprite_cursor_pointer, 0, 0)
+    }
+}
 function finish_walk_in_the_park_map () {
     land_tiles = [assets.tile`grass`, sprites.castle.tileGrass1, sprites.castle.tileGrass3, sprites.castle.tileGrass2]
     water_tiles = [
@@ -42,6 +53,19 @@ function set_walk_in_the_park_map () {
     scene.setBackgroundColor(7)
     tiles.setTilemap(tilemap`walk_in_the_park_map`)
 }
+function make_cursor () {
+    sprite_cursor = sprites.create(assets.image`cursor`, SpriteKind.Player)
+    sprite_cursor_pointer = sprites.create(assets.image`cursor_pointer`, SpriteKind.Player)
+    enable_cursor_movement(true)
+    sprite_cursor_pointer.setFlag(SpriteFlag.StayInScreen, true)
+    sprite_cursor_pointer.setFlag(SpriteFlag.GhostThroughWalls, true)
+    sprite_cursor.setFlag(SpriteFlag.Ghost, true)
+    update_cursor()
+    scene.cameraFollowSprite(sprite_cursor_pointer)
+}
+function game_init () {
+    make_cursor()
+}
 function set_map (index: number) {
     if (index == 0) {
         set_walk_in_the_park_map()
@@ -56,5 +80,11 @@ let bloon_paths: TilemapPath.TilemapPath[] = []
 let spawn_locations: tiles.Location[] = []
 let water_tiles: Image[] = []
 let land_tiles: Image[] = []
+let sprite_cursor_pointer: Sprite = null
+let sprite_cursor: Sprite = null
 set_map(0)
 finish_map_loading(0)
+game_init()
+game.onUpdate(function () {
+    update_cursor()
+})
