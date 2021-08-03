96756eadb33c3a42cc77233651cb8c8b919f7d07
Ckyiu <UnsignedArduino@outlook.com>
2021-08-02 20:56:42 -0400

    Finish map initialization for "Walk in the Park" map

--- main.ts
+++ main.ts
@@ -1,1 +1,60 @@
-
+function finish_walk_in_the_park_map () {
+    land_tiles = [assets.tile`grass`, sprites.castle.tileGrass1, sprites.castle.tileGrass3, sprites.castle.tileGrass2]
+    water_tiles = [
+    assets.tile`top_left_water`,
+    assets.tile`top_water`,
+    assets.tile`top_right_water`,
+    assets.tile`left_water`,
+    assets.tile`water`,
+    assets.tile`right_water`,
+    assets.tile`bottom_left_water`,
+    assets.tile`bottom_water`,
+    assets.tile`bottom_right_water`
+    ]
+    spawn_locations = [tiles.getTileLocation(2, 0), tiles.getTileLocation(3, 0)]
+    bloon_paths = []
+    bloon_paths.push(TilemapPath.create_path([
+    tiles.getTileLocation(2, 0),
+    tiles.getTileLocation(2, 3),
+    tiles.getTileLocation(16, 3),
+    tiles.getTileLocation(16, 11),
+    tiles.getTileLocation(8, 11),
+    tiles.getTileLocation(8, 3),
+    tiles.getTileLocation(16, 3),
+    tiles.getTileLocation(16, 11),
+    tiles.getTileLocation(2, 11),
+    tiles.getTileLocation(2, 14)
+    ]))
+    bloon_paths.push(TilemapPath.create_path([
+    tiles.getTileLocation(3, 0),
+    tiles.getTileLocation(3, 2),
+    tiles.getTileLocation(17, 2),
+    tiles.getTileLocation(17, 12),
+    tiles.getTileLocation(7, 12),
+    tiles.getTileLocation(7, 2),
+    tiles.getTileLocation(17, 2),
+    tiles.getTileLocation(17, 12),
+    tiles.getTileLocation(3, 12),
+    tiles.getTileLocation(3, 14)
+    ]))
+}
+function set_walk_in_the_park_map () {
+    scene.setBackgroundColor(7)
+    tiles.setTilemap(tilemap`walk_in_the_park_map`)
+}
+function set_map (index: number) {
+    if (index == 0) {
+        set_walk_in_the_park_map()
+    }
+}
+function finish_map_loading (index: number) {
+    if (index == 0) {
+        finish_walk_in_the_park_map()
+    }
+}
+let bloon_paths: TilemapPath.TilemapPath[] = []
+let spawn_locations: tiles.Location[] = []
+let water_tiles: Image[] = []
+let land_tiles: Image[] = []
+set_map(0)
+finish_map_loading(0)
---
+++ tilemap.g.ts
@@ -0,0 +1,135 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "walk_in_the_park_map":
+            case "level1":return tiles.createTilemap(hex`14000f00040105080101010101011701010101011901010101010508010101010101010101030101010104010101050b0a0a0a0a0a0a0a0a0a0a0a0a0a09010117010c060606060b0b060606060606060b0801010101010101010105080101010401170105080101010401010101170508010e0f0f0f1001050801010101010101040105080111121212130105080101011803010101010508021112121213010508011701010101170101050801111212121301050801010104010101010105080114151515160105080101010101010103010508010101180101010508030101010d0a0a0a0a0b0b0a0a0a0a0a0a0a0b0801010117050b0606060606060606060606060607010104010508010101170101040101010101010101010101050801010101010101010101170101010401`, img`
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile1,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.castle.tileGrass2,sprites.castle.tilePath4,sprites.castle.tilePath8,sprites.castle.tilePath9,sprites.castle.tilePath6,sprites.castle.tilePath3,sprites.castle.tilePath2,sprites.castle.tilePath5,sprites.castle.tilePath7,sprites.castle.tilePath1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,sprites.builtin.forestTiles0,sprites.castle.rock0,sprites.castle.rock1], TileScale.Sixteen);
+            case "level3":
+            case "level3":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level4":
+            case "level4":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "grass":
+            case "tile1":return tile1;
+            case "top_left_water":
+            case "tile2":return tile2;
+            case "top_water":
+            case "tile3":return tile3;
+            case "top_right_water":
+            case "tile4":return tile4;
+            case "left_water":
+            case "tile5":return tile5;
+            case "water":
+            case "tile6":return tile6;
+            case "right_water":
+            case "tile7":return tile7;
+            case "bottom_left_water":
+            case "tile8":return tile8;
+            case "bottom_water":
+            case "tile9":return tile9;
+            case "bottom_right_water":
+            case "tile10":return tile10;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
